85ae3c50d63c928d16e65829f80f822956db5069
Ingrid Mendonca <ingridmsf19@gmail.com>
2022-03-21 17:54:16 -0300

    Cap 2, pg 53

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,22 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        // cada parâmetro recebido será atribuído às propiedades da classe
+        this._data = data; //data atual
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+    getQuantidade() {
+        return this._quantidade;
+    }
+    getValor() {
+        return this._valor;
+    }
+}
