0c9dac56ca6a0362343545fced3450298c980576
Ingrid Mendonca <ingridmsf19@gmail.com>
2022-03-22 11:03:25 -0300

    Cap 3 - Iniciando  método Controller do MVC

---
+++ client/app/app.js
@@ -0,0 +1,6 @@
+// criando a instância do controller
+let controller = new NegociacaoController();
+
+// passando diretamente controller.adiciona
+
+document.querySelector('.form').addEventListener('submit', controller.adiciona);
