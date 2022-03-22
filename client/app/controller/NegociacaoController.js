68f3db2345f9e5bae4c25996c2e3afc317877719
Ingrid Mendonca <ingridmsf19@gmail.com>
2022-03-22 15:59:33 -0300

    Trabalhando com conversões de datas

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -12,12 +12,15 @@ class NegociacaoController {
     // cancelando a submissão do formulário
     event.preventDefault();
 
-    let negociacao = new Negociacao(DateConverter.paraData(this._inputData.value), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+    let negociacao = new Negociacao(
+      DateConverter.paraData(this._inputData.value),
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
 
     console.log(negociacao.data);
 
     let diaMesAno = DateConverter.paraTexto(negociacao.data);
     console.log(diaMesAno);
-
   }
 }
