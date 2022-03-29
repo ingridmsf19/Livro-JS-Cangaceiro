2ef5a66c6f8f85d49eb91eac88bedf4d7f84dc98
Ingrid Mendonca <ingridmsf19@gmail.com>
2022-03-29 16:32:45 -0300

    Finalizando cap 11 = pg 262

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -13,8 +13,8 @@ class DateConverter {
   static paraData(texto) {
 
       if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
-          throw new Error('Deve estar no formato aaaa-mm-dd');
+          throw new DataInvalidaException();
 
-      return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+      return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
   }
 }
